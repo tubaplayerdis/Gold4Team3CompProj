3253fdd4e01cd515f34de524b709fb8d19accb3e
Aaron Wilk <aaron.wilk10@gmail.com>
2024-10-15 12:46:37 -0500

    hiddenapiupdates

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 /*----------------------------------------------------------------------------*/
 
 #include "vex.h"
+#include "v5_api.h"
 #include "Bot.h"
 #include "Drivetrain.h"
 
@@ -91,6 +92,8 @@ int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
+
+  V5_DeviceT devices = vexDevicesGet();
 
   // Run the pre-autonomous function.
   pre_auton();
