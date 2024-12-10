925417c8a80ea9b88bfc3c43a770b1a6cab22013
Aaron Wilk <aaron.wilk@icloud.com>
2024-12-09 19:02:12 -0600

    add stuff

--- include/Skills.h
+++ include/Skills.h
@@ -1,19 +1,51 @@
 #include <vector>
 #include <vex.h>
 
+enum SkillsTaskType {
+    Driving = 0,
+    Segmenting = 1,
+    Turning = 2,
+    Mogo = 3,
+    Arm = 4,
+    EngGame = 5
+};
+
 
 struct SkillsTask {
-    double destx;
-    double desty;
-    double desth;
     const char name[17];
-    bool doDrive;
-    bool doSpecial;
-    bool toggleMogo;
-    int (*special)(int); //function pointer that takes an int as input and output. used for a special implementation for the task.
+    SkillsTaskType stype = SkillsTaskType::Driving;
+
+    //ALL VALUES OF DISTANCE ARE MM, ALL VALUES OF TURNING ARE DEGREES.
+
+    //If Driving.
+    double destx = -1;
+    double desty = -1;
+    double desth = -1;
+
+    //If Segmenting
+    double segAmount = -1;
+
+    //If Only Turning
+    double turnBy = -1;
+
+    //If Actuating Mogo
+    bool toggleMogoTo = false;
+
+    //If Actuating Arm
+    double turnArmTo = -1;
+
+    //Actuate Endgame
+    double spinMotorsDegrees;
+
     bool isComplete = false;
 };
 
+class SkillsEngine {
+    public:
+        static int onTaskIndex;
+        static void addTask(SkillsTask task);
+        static SkillsTask currentTask();
+};
 
 class Skills {
     private:
