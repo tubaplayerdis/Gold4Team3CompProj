45de116408a75f3233101f50ea789a2070609314
tubaplayerdis <70219635+tubaplayerdis@users.noreply.github.com>
2025-02-02 16:33:11 -0600

    update comments

--- include/Bot.h
+++ include/Bot.h
@@ -8,7 +8,7 @@
 #define AI_HEU_TOLERANCE 24 //1-40
 #define AI_SATURATION_TOLERANCE 0.41 //0.10-1
 #define LADYBROWN_DESIRED_ANGLE 22 // Via a rotation sensor on the arm, not the pinion.
-#define LADYBROWN_ANGLE_TOLERANCE 1 // Tolerance on the motor
+#define LADYBROWN_ANGLE_TOLERANCE 1 // Tolerance on the angle via arm roation sensor
 
 //BLUE
   //rgb(88,156,156)
