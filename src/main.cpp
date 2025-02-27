09f2719d1593780c2af73c53a322503c13d2c905
tubaplayerdis <70219635+tubaplayerdis@users.noreply.github.com>
2025-02-26 18:21:00 -0600

    pid tuning

--- src/main.cpp
+++ src/main.cpp
@@ -1477,7 +1477,7 @@ void usercontrol(void) {
 
 void ToggleLadyBrown() {
   Bot::IgnoreArm = true;
-  turnArmToPID(23.3);
+  turnArmToPID(25);
   Bot::IgnoreArm = false;
   //Bot::desiredARMAngle = LADYBROWN_DESIRED_ANGLE;
   //Bot::isArmPIDActive = !Bot::isArmPIDActive;
